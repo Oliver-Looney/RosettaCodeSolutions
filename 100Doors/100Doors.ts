63e9400fe124f9f68488260dc3d539a3e77c0b76
Oliver-Looney <Oliver.Looney@liberty-it.co.uk>
2021-09-01 15:21:31 +0100

    passed FCC's test

--- 100Doors/100Doors.ts
+++ 100Doors/100Doors.ts
@@ -1,21 +1,21 @@
-export function DoorsFunc(numDoors:number){
-    let result:number[]=[];
+export function DoorsFunc(numDoors: number) {
+    let result: number[] = [];
 
     function getNumOfDivisors(input: number) {
         let sum = 0;
-        for (let i=1;i<Math.sqrt(input);i++){
-            if(input%i===0){
-                sum+=2;
+        for (let i = 1; i < Math.sqrt(input); i++) {
+            if (input % i === 0) {
+                sum += 2;
             }
         }
-        if(input%Math.sqrt(input)===0){
+        if (input % Math.sqrt(input) === 0) {
             sum++;
         }
         return sum;
     }
 
-    for(let i=1;i<=numDoors;i++){
-        if(getNumOfDivisors(i)%2===1){
+    for (let i = 1; i <= numDoors; i++) {
+        if (getNumOfDivisors(i) % 2 === 1) {
             result.push(i);
         }
     }
