5781d46d9303eb0e7fb97890312833edc2af9698
Oliver-Looney <Oliver.Looney@liberty-it.co.uk>
2021-09-01 16:36:55 +0100

    created sumOfDivisorsOfNum

--- AmicablePairs/AmicablePairs.ts
+++ AmicablePairs/AmicablePairs.ts
@@ -1,14 +1,25 @@
-
-
-function getNumbersAndSumOfDivisors(maxNum: number) {
-    let result = new Map<number,number>();
-    for(let i=6;i<=maxNum;i++){
-        result.set(i,sumDivisorsOfNum(i));
+function sumDivisorsOfNum(input: number) {
+    let result = 1;
+    for (let i = 2; i < Math.sqrt(input); i++) {
+        if (input % i === 0) {
+            result += i + input / i;
+        }
+    }
+    if (input % Math.sqrt(input) === 0) {
+        result += Math.sqrt(input);
     }
     return result;
 }
 
-function AmicablePairsFunc(maxNum:number){
+function getNumbersAndSumOfDivisors(maxNum: number) {
+    let result = new Map<number, number>();
+    for (let i = 6; i <= maxNum; i++) {
+        result.set(i, sumDivisorsOfNum(i));
+    }
+    return result;
+}
+
+function AmicablePairsFunc(maxNum: number) {
     let myNumbers = getNumbersAndSumOfDivisors(maxNum);
 }
 
