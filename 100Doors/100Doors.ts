2acc55907773c51dc2a7489349ef3216dab136e4
Oliver-Looney <Oliver.Looney@liberty-it.co.uk>
2021-09-01 15:16:19 +0100

    first attempt of soln

---
+++ 100Doors/100Doors.ts
@@ -0,0 +1,24 @@
+export function DoorsFunc(numDoors:number){
+    let result:number[]=[];
+
+    function getNumOfDivisors(input: number) {
+        let sum = 0;
+        for (let i=1;i<Math.sqrt(input);i++){
+            if(input%i===0){
+                sum+=2;
+            }
+        }
+        if(input%Math.sqrt(input)===0){
+            sum++;
+        }
+        return sum;
+    }
+
+    for(let i=1;i<=numDoors;i++){
+        if(getNumOfDivisors(i)%2===1){
+            result.push(i);
+        }
+    }
+
+    return result;
+}
